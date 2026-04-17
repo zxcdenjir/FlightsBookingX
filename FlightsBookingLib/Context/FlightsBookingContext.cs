2e9c3abfed570bff5b290f4c6996ae264bdaf03e
zxcdenjir <rush.wars1592@gmail.com>
2026-04-17 09:27:15 +0300

    commit

--- FlightsBookingLib/Context/FlightsBookingContext.cs
+++ FlightsBookingLib/Context/FlightsBookingContext.cs
@@ -34,7 +34,7 @@ public partial class FlightsBookingContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("host=10.228.48.236; Username=postgres; Database=FlightsBooking; Password=123");
+        => optionsBuilder.UseNpgsql("host=localhost; Username=postgres; Database=FlightsBooking; Password=123");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
